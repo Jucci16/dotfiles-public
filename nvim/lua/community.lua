0ca2373b54d63091a52580d6ad3a32cca61f1e5c
John Ucci <john.ucci3@gmail.com>
2024-08-10 12:41:25 -0400

    Added Lua Language support

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,9 +5,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
